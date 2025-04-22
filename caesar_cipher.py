138cc565a9254e3d6fa057f9593a3783fc585180
Joel Prashanth <142143829+joel-prashanth@users.noreply.github.com>
2025-04-22 09:30:29 +0530

    Finish decode logic

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,6 +1,8 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
             'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print("Welcome to Caesar Cipher!")
+
 direction = input(
     "Type 'encode' to encrypt and 'decode' to decrypt your message: ").lower().strip()
 
@@ -21,12 +23,31 @@ def encrypt(original_text, shift_key):
             encoded_alphabet = alphabet[shifted_index]
             encoded_word += encoded_alphabet
         else:
-             encoded_word += char
+            encoded_word += char
 
     return encoded_word
 
-encrypted_word = encrypt(text, shift)
-print(encrypted_word)
+
+def decrypt(encrypted_word, key):
+
+    decoded_word = ""
+    for char in encrypted_word:
+        if (char in alphabet):
+            encoded_index = alphabet.index(char)
+            decode_index = encoded_index - key
+            decoded_word += alphabet[decode_index]
+        else:
+            decoded_word += char
+
+    print(f"The original message was: {decoded_word}")
+
 
 if (direction == 'encode'):
-    encrypt
+    encrypted_word = encrypt(text, shift)
+    print(f"Message encoded to: {encrypted_word}")
+
+elif (direction == 'decode'):
+    decrypted_word = decrypt(text, shift)
+
+else:
+    print("Enter correct option.")
